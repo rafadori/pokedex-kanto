708b1df72ff3029d14a7388000d4b0960b9ac64f
rafadori <rafadori26@gmail.com>
2024-03-04 21:26:47 -0300

    optimize page loading

--- script.js
+++ script.js
@@ -1,46 +1,68 @@
-fetch("https://pokeapi.co/api/v2/pokemon?limit=151") // endpoint que irá nos retornar apenas os pokémons de primeira geração
-  .then((response) => {
-    return response.json()
+const typeColors = {
+  grass: "#5cb737",
+  poison: "#88447a",
+  fire: "#d52100",
+  flying: "#556dff",
+  water: "#0080ff",
+  bug: "#83901a",
+  normal: "#797964",
+  electric: "#c90",
+  ground: "#bf9926",
+  fighting: "#a84d3d",
+  psychic: "#ff227a",
+  rock: "#a59249",
+  steel: "#8e8ea4",
+  ice: "#0af",
+  ghost: "#5454b3",
+  dragon: "#4e38e9",
+}
+
+async function fetchPokemonData(pokemonId) {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+  const data = await response.json()
+  return {
+    name: data.name,
+    image: data.sprites.front_default,
+    types: data.types.map((type) => type.type.name),
+  }
+}
+
+async function fetchAllPokemonData() {
+  const promises = []
+  Array.from({ length: 151 }, (_, index) => index + 1).forEach((pokemonId) => {
+    promises.push(fetchPokemonData(pokemonId))
   })
-  .then(async (data) => {
-    console.log(data.results)
-    const page = document.querySelector("#pokedex-page")
-    const box = document.querySelector("#pokemon-box")
-    page.innerHTML = ""
-    for (let i = 0; i < data.results.length; i++) {
-      // inserindo os nomes dos pokemons
-      box.querySelector("#pokemon-name").innerHTML = data.results[i].name
-      box.querySelector("#pokemon-name").style.textTransform = "capitalize"
+  return Promise.allSettled(promises)
+}
 
-      // inserindo os tipos dos pokemons
-      const pokemonType = await fetch(
-        "https://pokeapi.co/api/v2/pokemon/" + data.results[i].name
-      )
-      const type = await pokemonType.json()
+async function displayPokemonData() {
+  const pokemonContainer = document.getElementById("pokemonContainer")
+  const results = await fetchAllPokemonData()
 
-      function setTypeClass(elementId, typeName) {
-        const element = box.querySelector(elementId)
-        element.innerHTML = typeName
-        element.setAttribute("class", typeName)
-      }
+  results.forEach((result) => {
+    if (result.status === "fulfilled") {
+      const pokemon = result.value
+      const pokemonDiv = document.createElement("div")
+      pokemonDiv.classList.add("pokemon")
 
-      if (type.types.length == 1) {
-        setTypeClass("#pokemon-type", type.types[0].type.name)
-        box.querySelector("#pokemon-type-d").innerHTML = ""
-      } else {
-        setTypeClass("#pokemon-type", type.types[0].type.name)
-        setTypeClass("#pokemon-type-d", type.types[1].type.name)
-      }
+      // Adiciona a cor correspondente ao tipo do pokemon
+      const typesWithColors = pokemon.types.map((type) => {
+        const color = typeColors[type]
+        return `<span style="color: ${color};">${type}</span>`
+      })
 
-      box.querySelector("#pokemon-type").style.textTransform = "capitalize"
-      box.querySelector("#pokemon-type-d").style.textTransform = "capitalize"
-
-      // inserindo imagem dos pokemons
-      const pokemonImage = await fetch(
-        "https://pokeapi.co/api/v2/pokemon-form/" + data.results[i].name
-      )
-      const image = await pokemonImage.json()
-      box.querySelector("#pokemon-img").src = image.sprites.front_default
-      page.innerHTML += box.outerHTML
+      pokemonDiv.innerHTML = `
+                <img src="${pokemon.image}" alt="${pokemon.name}">
+                <h3>${pokemon.name}</h3>
+                <p>${typesWithColors.join(" ")}</p>
+            `
+      pokemonContainer.appendChild(pokemonDiv)
+    } else {
+      const errorDiv = document.createElement("div")
+      errorDiv.textContent = `Falha ao carregar pokemon`
+      pokemonContainer.appendChild(errorDiv)
     }
   })
+}
+
+displayPokemonData()
